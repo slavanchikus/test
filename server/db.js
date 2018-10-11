b218038e37815ca8dd6535c1f0f2567cd4de9f26
Вячеслав Беляков <beliakov@company.dnevnik.ru>
2018-10-11 17:47:13 +0300

    :)

--- server/db.js
+++ server/db.js
@@ -1,6 +1,8 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('postgres://eggrgvib:H5xqLV7wV2CpfaPGuREvPE-N73uSlsGh@dumbo.db.elephantsql.com:5432/eggrgvib');
+const sequelize = new Sequelize('postgres://eggrgvib:H5xqLV7wV2CpfaPGuREvPE-N73uSlsGh@dumbo.db.elephantsql.com:5432/eggrgvib', {
+  operatorsAliases: Sequelize.Op
+});
 
 const db = {};
 
